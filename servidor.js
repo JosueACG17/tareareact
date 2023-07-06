e41b5e5c07bdfaa533e746fb1a4316ad3f2e6dd3
JosueACG17 <95649316+JosueACG17@users.noreply.github.com>
2023-07-06 12:52:19 -0500

    Add files via upload

--- servidor.js
+++ servidor.js
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { json } from "express";
 import mysql from 'mysql';
 import cors from "cors";
+import jwt from "jsonwebtoken";
 
 //CREA LA INSTANCIA DE EXPRESS
 
@@ -30,14 +31,31 @@ conexion.connect(function (error){
 
 app.get('/obtenerMascotas',(peticion, respuesta) =>{
     const sql = "SELECT * FROM mascotas";
+    console.log(peticion.body)
     conexion.query(sql,(error,resultado) => {
         if (error) return respuesta.json ({respuesta: "error"});
         return respuesta.json({mensaje:"exitoso", contenido:resultado})
     });
 });
 
-//INICIAMOS EL SERVIDOR 
 
+// ACCESO A USUARIO
+app.post('/acceso', (peticion, respuesta) =>{
+    const sql= "SELECT * FROM usuarios where correo_electronico = ? AND contrasenia = ?"
+    console.log(peticion.body);
+    conexion.query(sql, [peticion.body.correo_electronico, peticion.body.contrasenia], (error, resultado) => {
+        if (error) return respuesta.json({Error: "Error en la consulta"});
+        if (resultado.lenght > 0) {
+            const token = jwt.sign({usuario: "administrador"}, "12", {expiresIn: '1d'});
+            respuesta.cookie(token);
+            return respuesta.json({Estatus: "exitoso", usuario:token})
+        } else {
+            return respuesta.json({Estatus: "error", Error: "Usuario o Contraseña Incorrecta"})
+        }
+    })
+})
+
+//INICIAMOS EL SERVIDOR 
 app.listen(8082, ()=>{
     console.log("Servidor Iniciado");
 });
